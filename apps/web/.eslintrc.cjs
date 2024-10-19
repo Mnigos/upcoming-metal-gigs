2567f8c975f3eff808fce7586e5ab9a77c2a66d1
Mnigos <makowskiigor@gmail.com>
2024-10-19 09:18:04 +0200

    chore(apps/web/eslintrc): disable `consistent-type-definitions` and `array-type` for sanity generated types

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,5 +1,14 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.cjs"],
-};
+  extends: ['@repo/eslint-config/next.cjs'],
+  overrides: [
+    {
+      files: ['**/sanity/types.ts'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/array-type': 'off',
+      },
+    },
+  ],
+}
