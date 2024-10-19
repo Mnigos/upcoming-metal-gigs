2055da3311cc097af599ebde990fb2459a75381d
Mnigos <makowskiigor@gmail.com>
2024-10-19 08:07:14 +0200

    feat(apps/web): initialize sanity

---
+++ apps/web/lib/sanity/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from 'next-sanity'
+
+import { env } from '../env'
+
+export const client = createClient({
+  projectId: env.SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: false,
+})
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
