ac46d4b6d5cd70e463e7b4e6a9e4baa5bc88e921
Mnigos <makowskiigor@gmail.com>
2024-10-19 09:16:56 +0200

    feat(apps/web/next.config): add `fbcdn` to `remotePatterns`

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.xx.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
